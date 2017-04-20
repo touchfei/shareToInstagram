6bc54b7aef9517b275f22e1c4ea2e4088beaf961
gaofei <gaofei@formssi.com>
2017-04-20 17:18:42 +0800

    加入子线程 写入数据 生成图片名称

--- share/ViewController.m
+++ share/ViewController.m
@@ -22,7 +22,7 @@
     
 //    [self loadInstagramByWebview];
     [self loadInstagram];
-//    [self shareByDocument];
+    [self shareToInstagramByImage:@"my_qq"];
     
 }
 
@@ -34,7 +34,7 @@
     [self.view addSubview:web];
 }
 
-
+#pragma mark - instagram share
 
 /**
  如果你的应用使用了如SSO授权登录或跳转到第三方分享功能，
@@ -58,52 +58,72 @@
 }
 
 
--(void)shareByDocument{
-    UIImage *image = [UIImage imageNamed:@"my_qq"];
-   NSString *imagesPath = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES).firstObject;
-    NSString* filename = [NSString stringWithFormat:@"testImage.igo"];
-    NSString* savePath = [imagesPath stringByAppendingPathComponent:filename];
-    [UIImagePNGRepresentation(image) writeToFile:savePath atomically:YES];
+
+-(void)shareToInstagramByDocumentImage:(NSString *)imageStr{
+    
+    NSURL *instagramURL = [NSURL URLWithString:@"instagram://app"];
+    
+    if ([[UIApplication sharedApplication] canOpenURL:instagramURL]){// 判断是否安装了Instagram
+        
+        dispatch_async(dispatch_get_global_queue(0, 0), ^{// 子线程写入数据
+            
+            UIImage *image = [UIImage imageNamed:imageStr];
+            NSString *name = [NSString stringWithFormat:@"%f%u.igo",[NSDate timeIntervalSinceReferenceDate],arc4random_uniform(1000)];
+            NSString *savePath = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES).firstObject stringByAppendingPathComponent:name];
+            
+            [UIImagePNGRepresentation(image) writeToFile:savePath atomically:YES];
+            dispatch_async(dispatch_get_main_queue(), ^{// 主线程显示视图
+                self.documentInteractionController = [UIDocumentInteractionController interactionControllerWithURL:[NSURL fileURLWithPath:savePath]];
+                self.documentInteractionController.UTI = @"com.instagram.exclusivegram";
+                self.documentInteractionController.delegate = self;
+                [self.documentInteractionController presentOpenInMenuFromRect:CGRectZero inView:self.view animated:YES];
+            });
+        });
+        
+    }else{
+        NSLog(@"did not install instagram");
+        [self showAlterWithStr:@"did not install instagram"];
+    }
+}
+
+-(void)shareToInstagramByImage:(NSString *)imageStr{
     
     NSURL *instagramURL = [NSURL URLWithString:@"instagram://app"];
     
     if ([[UIApplication sharedApplication] canOpenURL:instagramURL]){
-        self.documentInteractionController = [UIDocumentInteractionController interactionControllerWithURL:[NSURL fileURLWithPath:savePath]];
-        self.documentInteractionController.UTI = @"com.instagram.exclusivegram";
-        self.documentInteractionController.delegate = self;
-        [self.documentInteractionController presentOpenInMenuFromRect:CGRectZero inView:self.view animated:YES];
+        
+        dispatch_async(dispatch_get_global_queue(0, 0), ^{
+            
+            UIImage *image = [UIImage imageNamed:imageStr];
+            NSString *name = [NSString stringWithFormat:@"%f%u.ig",[NSDate timeIntervalSinceReferenceDate],arc4random_uniform(1000)];
+            NSString *savePath = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES).firstObject stringByAppendingPathComponent:name];
+            
+            [UIImagePNGRepresentation(image) writeToFile:savePath atomically:YES];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                self.documentInteractionController = [UIDocumentInteractionController interactionControllerWithURL:[NSURL fileURLWithPath:savePath]];
+                self.documentInteractionController.UTI = @"com.instagram.photo";
+                self.documentInteractionController.delegate = self;
+                [self.documentInteractionController presentOpenInMenuFromRect:CGRectZero inView:self.view animated:YES];
+            });
+        });
+        
     }else{
         NSLog(@"did not install instagram");
-    }
-}
-
--(void)shareByDocument2{
-    UIImage *image = [UIImage imageNamed:@"my_qq"];
-    NSString *imagesPath = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES).firstObject;
-    NSString* filename = [NSString stringWithFormat:@"testImage.ig"];
-    NSString* savePath = [imagesPath stringByAppendingPathComponent:filename];
-    [UIImagePNGRepresentation(image) writeToFile:savePath atomically:YES];
-    
-    NSURL *instagramURL = [NSURL URLWithString:@"instagram://app"];
-    
-    if ([[UIApplication sharedApplication] canOpenURL:instagramURL]){
-        self.documentInteractionController = [UIDocumentInteractionController interactionControllerWithURL:[NSURL fileURLWithPath:savePath]];
-        self.documentInteractionController.UTI = @"com.instagram.photo";
-        self.documentInteractionController.delegate = self;
-        [self.documentInteractionController presentOpenInMenuFromRect:CGRectZero inView:self.view animated:YES];
-    }else{
-        NSLog(@"did not install instagram");
+        [self showAlterWithStr:@"did not install instagram"];
     }
 }
 
 
--(void)shareByDocument3{
-
+-(void)shareToInstagramByDocumentnNameWith:(NSString *)imageName{
+    
     NSURL *instagramUrl = [NSURL URLWithString:@"instagram://app"];
-    NSString  *jpgPath = [[NSBundle mainBundle] pathForResource:@"my_qq.png" ofType:nil];
-    NSURL *imageUrl = [[NSURL alloc] initWithString:[[NSString alloc] initWithFormat:@"file://%@", jpgPath]];
+    
     
     if ([[UIApplication sharedApplication] canOpenURL:instagramUrl]){
+        
+        NSString  *jpgPath = [[NSBundle mainBundle] pathForResource:imageName ofType:nil];
+        NSURL *imageUrl = [[NSURL alloc] initWithString:[[NSString alloc] initWithFormat:@"file://%@", jpgPath]];
+        
         self.documentInteractionController = [UIDocumentInteractionController interactionControllerWithURL:imageUrl];
         self.documentInteractionController.UTI = @"com.instagram.photo";
         self.documentInteractionController.delegate = self;
@@ -111,8 +131,32 @@
         [self.documentInteractionController presentOpenInMenuFromRect:CGRectZero inView:self.view animated:YES];
     }else{
         NSLog(@"did not install instagram");
+        [self showAlterWithStr:@"did not install instagram"];
     }
 }
 
+#pragma mark - IDocumentInteractionControllerDelegate
+
+-(void)documentInteractionController:(UIDocumentInteractionController *)controller didEndSendingToApplication:(NSString *)application{
+    NSLog(@"%s",__func__);
+    // 判断是否返回应用
+}
+
+- (void)documentInteractionControllerWillPresentOpenInMenu:(UIDocumentInteractionController *)controller{
+    NSLog(@"%s",__func__);
+}
+- (void)documentInteractionControllerDidDismissOpenInMenu:(UIDocumentInteractionController *)controller{
+    NSLog(@"%s",__func__);
+}
+
+#pragma mark - show error
+
+- (void)showAlterWithStr:(NSString *)str{
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:nil message:str preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleCancel handler:nil];
+    [alertController addAction:cancelAction];
+    
+    [self presentViewController:alertController animated:YES completion:nil];
+}
 
 @end
